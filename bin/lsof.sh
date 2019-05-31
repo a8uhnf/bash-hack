0311388999ce8b778fce37d55756f511a456fecf
hanif <abuhanifa@pathao.com>
2019-06-01 03:06:39 +0600

    add lsof

---
+++ bin/lsof.sh
@@ -0,0 +1,8 @@
+$ lsof <file-or-dir-path> # see which processes use this file or dir
+
+$ lsof -p <process-number> # see some process's related file
+$ lsof -u <username> # see user related files
+
+$ lsof -i :80
+
+$ lsof -i TCP
