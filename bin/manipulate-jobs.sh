5f38d22f0886925bebfeb612c280a4771f27ca20
hanif <abuhanifa@pathao.com>
2019-07-06 02:49:15 +0600

    manipulate processes

---
+++ bin/manipulate-jobs.sh
@@ -0,0 +1,8 @@
+$ ps -f # process table
+
+$ sleep 360 # run a job
+$ ctrl + z # suspend the process 
+$ jobs # to see the 
+$ fg # run jobs in foregroung 
+$ bg # run jobs in background 
+$ fg %<job-number> # run job-number in foreground
