11152ab87ce9d3c6d4a12e1080101038cb1bbe61
hanif <abuhanifa@pathao.com>
2020-11-03 01:27:32 +0600

    Add telnet command to find out the idle timeout of a server

--- bin/netstat.sh
+++ bin/netstat.sh
@@ -1,2 +1,16 @@
 netstat -nplt # -p=show PID, -n=numeric values instead of host, -l=only show listen address, -t=tcp protocol
 netstat -cnplt # -c=monitor continuously
+
+# display dropped packets per interface 
+netstate -i 
+netstate --interfaces
+
+# Dispaly summury of each protocol
+netstate --statistics
+netstate -s
+
+# TCP state
+netstate -s --tcp
+
+# UDP state
+netstate -s --udp
---
+++ bin/telnet.sh
@@ -0,0 +1,3 @@
+# run this command and start a stop watch.
+# keep a watch when this connection got broke. this is the timeout of this specific server
+telnet <domain-name> <port> 
